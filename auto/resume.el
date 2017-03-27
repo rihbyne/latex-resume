c87423fa7336f4398f576866fb95eaa15395cedc
rihbyne <rihen234@gmail.com>
2017-03-27 19:22:49 +0530

    revised resume

--- auto/resume.el
+++ auto/resume.el
@@ -1,21 +1,19 @@
 (TeX-add-style-hook
  "resume"
  (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("deedy-resume-openfont" "hidelinks")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "deedy-resume-openfont"
     "deedy-resume-openfont10"
     "fancyhdr"
     "article"
-    "art10")))
+    "art10"))
+ :latex)
 
